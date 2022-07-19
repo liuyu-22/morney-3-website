0f0cce929d7dcf7aefc66b4c1aef88b2bb7415a4
liuyu-22 <729467909@qq.com>
2022-07-19 22:41:20 +0800

    deploy

---
+++ precache-manifest.a29ddf6be589a211b4f0dfb23111fb06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d62199f041a5985b200e",
+    "url": "/morney-3-website/css/app.88510532.css"
+  },
+  {
+    "revision": "764fe5953a1f3bdeaf5ce24cd92ba27d",
+    "url": "/morney-3-website/index.html"
+  },
+  {
+    "revision": "d62199f041a5985b200e",
+    "url": "/morney-3-website/js/app.09c8f0cb.js"
+  },
+  {
+    "revision": "02597d1d9d04c379bbb5",
+    "url": "/morney-3-website/js/chunk-vendors.a3901e94.js"
+  },
+  {
+    "revision": "f3aaee380deddea3de78c3de33e81ce5",
+    "url": "/morney-3-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-3-website/robots.txt"
+  }
+]);
